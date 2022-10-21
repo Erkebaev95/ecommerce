95c1fc2ff556ccbc041d1464a787d7d016253427
Erkebaev Nursultan <73390365+Erkebaev95@users.noreply.github.com>
2022-10-21 19:05:11 +0600

    add package for entities

---
+++ src/main/java/com/erkebaev/dnsshop/DnsShopApplication.java
@@ -0,0 +1,13 @@
+package com.erkebaev.dnsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnsShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DnsShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/erkebaev/dnsshop/DnsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erkebaev.dnsshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnsShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
